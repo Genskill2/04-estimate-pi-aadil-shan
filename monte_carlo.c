f5b192cb3b51e9891e81b455c965e281091e1707
aadil-shan <85333190+aadil-shan@users.noreply.github.com>
2021-06-04 13:31:06 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -4,7 +4,16 @@
 #include <math.h>
 
 float mc_pi(int);
-
+{float pi=1.0;
+for (int i=1;i<n; i++)
+{
+int m;
+m=4.0*i*i;
+pi=(float) ((pi*m)/(m-1));
+}
+pi=pi *2.0;
+return pi;
+}
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
