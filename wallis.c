15b73a202eeb521fff84925afed131c477bf0c23
aadil-shan <85333190+aadil-shan@users.noreply.github.com>
2021-06-04 13:54:58 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,16 +4,7 @@
 #include <math.h>
 
 float wallis_pi(int);
-{float pi=1.0;
-for (int i=1;i<n; i++)
-{
-int m;
-m=4.0*i*i;
-pi=(float) ((pi*m)/(m-1));
-}
-pi=pi *2.0;
-return pi;
-}
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
@@ -31,5 +22,18 @@ int main(void) {
       abort();
     }
   }
+}
+
+float wallis_pi(int n)
+{
+int i;
+float pi=1,nr,dr;
+for(i=1;i<=n;i++)
+ {
+  nr=(4*(i*i));
+  dr=(4*(i*i)-1);
+  pi*=nr/dr;
+ }
+ return 2*pi;
 }
 
